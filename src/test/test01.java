3efff783e95d6d11e345dcd333c3cfb860154c0d
baekseung <dodomom771@gmail.com>
2023-03-09 14:37:18 +0900

    html parsing

--- src/test/test01.java
+++ src/test/test01.java
@@ -1,8 +1,28 @@
 package test;
 
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.Connection;
+ 
 public class test01 {
-	public static void main(String args[]) {
-		System.out.println("Hello 11");
-		System.out.println("Hello 11");
-	}
+    public static void main(String args[]){
+        try {
+            // 1. 수집 대상 URL
+            String URL = "https://heodolf.tistory.com/18";
+            
+            // 2. Connection 생성
+            Connection conn = Jsoup.connect(URL);
+ 
+            // 3. HTML 파싱.
+            Document html = conn.get(); // conn.post();
+            
+            // 4. HTML 출력
+            System.out.println( html.toString() ); 
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
