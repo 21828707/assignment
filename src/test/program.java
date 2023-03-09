98380b4e48a22668f1b3d68b42e2d1c8dbefbc24
baekseung <dodomom771@gmail.com>
2023-03-09 15:33:36 +0900

    incomplete1

---
+++ src/test/program.java
@@ -0,0 +1,16 @@
+package test;
+
+public class program {
+	private String html;
+	private int i;
+	
+	public program(String u) {
+		this.html = u;
+	}
+	
+	public void check() {
+		for(i = 0; ;i++) {
+			
+		}
+	}
+}
--- src/test/test01.java
+++ src/test/test01.java
@@ -10,7 +10,7 @@ public class test01 {
     public static void main(String args[]){
         try {
             // 1. 수집 대상 URL
-            String URL = "https://heodolf.tistory.com/18";
+            String URL = "http://192.168.100.128/";
             
             // 2. Connection 생성
             Connection conn = Jsoup.connect(URL);
